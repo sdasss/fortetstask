3663ba9547dc6b9a82b76e2706d264bf584331a9
sdasss <barabanowicktor@mail.ru>
2020-02-10 13:42:14 +0300

    first commit

---
+++ Literally/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Literally
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Новый Поиск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(197, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Пауза";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 230);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Продолжить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(197, 92);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(197, 118);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(197, 145);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 7;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(326, 38);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(725, 220);
+            this.treeView1.TabIndex = 8;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView1_AfterSelect);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Путь:";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(72, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Шаблон:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(72, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Символы:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 121);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Счётчик:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(72, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Таймер:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(326, 277);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(725, 20);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(194, 277);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(107, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Файл в обработке: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1113, 364);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Literally/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+
+namespace Literally
+{
+
+
+    public partial class Form1 : Form
+    {
+        int h = 1;
+        int z = -1;
+        int vbn = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Properties.Settings.Default.Save_text1;
+            textBox2.Text = Properties.Settings.Default.Save_text2;
+            textBox3.Text = Properties.Settings.Default.Save_text3;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+        }
+
+        private async void mayun()
+        {
+            bool bvn = false;
+            List<string> puti = new List<string>();
+            TreeNode LastNode = null;
+        Bruacingtelo:
+            if (vbn == 1)
+            {
+                treeView1.Nodes.Clear();
+                vbn = 0;
+                LastNode = null;
+                puti.Clear();
+            }
+
+
+            Stopwatch sw = new Stopwatch();
+            z = 0;
+
+
+            Properties.Settings.Default.Save_text1 = textBox1.Text;
+            Properties.Settings.Default.Save_text2 = textBox2.Text;
+            Properties.Settings.Default.Save_text3 = textBox3.Text;
+            Properties.Settings.Default.Save();
+
+
+            textBox4.Text = Convert.ToString(z);
+            string FileDire = textBox1.Text;
+            string Tamplat = textBox2.Text;
+            string FandTaxt = textBox3.Text;
+
+            treeView1.Nodes.Clear();
+
+                sw.Start();
+            foreach (var findedFile in Directory.EnumerateFiles(FileDire, Tamplat, SearchOption.AllDirectories))
+            {
+                textBox6.Text = findedFile;
+                var dt17 = DateTime.Now;
+                while (h == 0)
+                {
+                    await Task.Delay(1);
+
+
+
+                    TimeSpan ts1 = sw.Elapsed;
+                    textBox5.Text = Convert.ToString(ts1);
+
+
+
+
+                }
+
+
+                textBox4.Text = Convert.ToString(z);
+                if (vbn == 1) goto Bruacingtelo;
+
+                TimeSpan ts = sw.Elapsed;
+
+                textBox5.Text = Convert.ToString(ts);
+
+
+                await Task.Delay(1);
+
+
+
+
+
+                string tmp = File.ReadAllText(findedFile);
+                if (tmp.Contains(FandTaxt))
+                {
+
+                    string PolnyPutj = string.Empty;
+                    foreach (string Putj in findedFile.Split('\\'))
+                    {
+
+                        TimeSpan ts1 = sw.Elapsed;
+
+                        textBox5.Text = Convert.ToString(ts1);
+                        PolnyPutj = PolnyPutj + Putj + '\\';
+                        bvn = false;
+                        if (LastNode != null && puti.Contains(PolnyPutj))
+
+                            bvn = true;
+
+
+                        if (!puti.Contains(PolnyPutj)) puti.Add(PolnyPutj);
+                        if (LastNode == null)
+                            LastNode = treeView1.Nodes.Add(PolnyPutj, Putj);
+                        else
+                        {
+                            if (bvn == true)
+                            {
+                                TreeNode[] Founded = treeView1.Nodes.Find(PolnyPutj, true);
+                                LastNode = Founded[0];
+                            }
+                            else
+                            {
+                                LastNode = LastNode.Nodes.Add(PolnyPutj, Putj);
+                            }
+                        }
+
+
+
+
+                    }
+
+                }
+
+
+
+
+                TimeSpan ts2 = sw.Elapsed;
+                textBox5.Text = Convert.ToString(ts2);
+
+
+                z += 1;
+
+                textBox4.Text = z.ToString();
+
+
+            }
+
+
+
+            sw.Stop();
+
+            TimeSpan ts3 = sw.Elapsed;
+            textBox5.Text = Convert.ToString(ts3);
+            z = -1;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+
+        }
+
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            h = 1;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+            button3.Enabled = false;
+            if (z == -1)
+                mayun();
+            else
+            {
+                vbn = 1;
+                h = 1;
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            h = 0;
+            button3.Enabled = true;
+            button2.Enabled = false;
+            button1.Enabled = true;
+        }
+
+
+
+
+
+        private void TreeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
